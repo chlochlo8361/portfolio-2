f5a6bee0233f2bdf5510d9bddde5602b13c76ff0
chlochlo8361 <cjaven@gmail.com>
2021-04-16 23:57:10 -0700

    more changes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const searchBtn = document.querySelector('.search-btn')
 const image = document.querySelector('.image')
 const description = document.querySelector('.description')
 const results = document.querySelector('.results')
+const animeContainer = document.querySelector('.anime-container')
 
 // Event Listeners
 searchBtn.addEventListener('click', getAnime)
@@ -13,22 +14,16 @@ function getAnime() {
     fetch(`https://kitsu.io/api/edge/anime?filter[text]=${searchBox.value.trim()}`)
     .then(response => response.json())
     .then(result => {
-        let html = '';
+        let html = document.querySelector('.anime-container');
         if(result.data) {
             result.data.forEach(item => {
-                html += `
-                <div class = "anime-item">
-                    <div class = "anime-img">
-                        <img src = "${item.attributes.posterImage.medium}" alt = "food">
+                animeContainer.innerHTML += `
+                    <div class = "anime-img-container">
+                        <h3 class="anime-title">${item.attributes.titles.en}</h3>
+                        <img class="anime-img" src = "${item.attributes.posterImage.medium}" alt = "food">
                     </div>
-                    <div class = "anime-name">
-                        <h3>${item.attributes.titles.en}</h3>
-                    </div>
-                </div>
                 `;
             })
-            results.innerHTML = html;
         }
     }
-
 )}
