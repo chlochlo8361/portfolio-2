b2e496e4bf8e44c47d412ea1076cb6680c7a7466
chlochlo8361 <cjaven@gmail.com>
2021-04-19 20:12:37 -0700

    changes

--- app.js
+++ app.js
@@ -2,18 +2,14 @@
 const searchBox = document.querySelector('.search-box')
 const searchBtn = document.querySelector('.search-btn')
 const image = document.querySelector('.image')
-const description = document.querySelector('.description')
-const extraDetailsContainer = document.querySelector('.extra-details-container')
 const animeContainer = document.querySelector('.anime-container')
-const details = document.querySelector('.details-btn')
+var animeStore;
+
 
 // Event Listeners
 searchBtn.addEventListener('click', getAnime)
 searchBtn.addEventListener('mouseOver', hoverOn)
 searchBtn.addEventListener('mouseOut', hoverOff)
-// details.addEventListener('mouseOut', hoverOff)
-// details.addEventListener('mouseOver', hoverOn)
-// details.addEventListener('click', getDetails)
 
 // Functions
 function getAnime() {
@@ -22,18 +18,25 @@ function getAnime() {
     .then(result => {
         let html = document.querySelector('.anime-container');
         if(result.data) {
-            result.data.forEach(item => {
+            result.data.forEach((item,idx) => {
+
                 animeContainer.innerHTML += `
                     <div class = "anime-img-container">
                         <h3 class="anime-title">${item.attributes.titles.en}</h3>
                         <img class="anime-img" src = "${item.attributes.posterImage.medium}" alt = "food"></br>
-                        <button class="search-btn details-btn" onmouseover="hoverOn()" onmouseout="hoverOff()">Details</button>
+                        <button class="search-btn details-btn" onclick="getDetails(${idx})">Details</button>
                     </div>
                 `;
             })
+            animeStore = result.data;
+            const detailsBtn = document.querySelector('.details-btn')
+            // detailsBtn.addEventListener('mouseOut', hoverOff)
+            // detailsBtn.addEventListener('mouseOver', hoverOn)
+            detailsBtn.addEventListener('click', getDetails)
         }
     }
 )}
+
 
 function hoverOn() {
     searchBtn.style.fontSize = '1.7rem';
@@ -43,3 +46,31 @@ function hoverOff() {
     searchBtn.style.fontSize = '1.5rem';
     searchBtn.style.backgroundColor = 'rgb(25, 164, 189)';
 }
+
+function getDetails(idx) {    
+    // alert("You clicked the details button") 
+// };
+    const extraDetailsContainer = document.querySelector('.extra-details-container')
+    let item = animeStore[idx]
+    extraDetailsContainer.innerHTML += `
+            <div class = "extra-details">
+                <button class="close-btn" onclick="closeExtraDetails()">&#10006;</button>
+                <h3 class="anime-title">${item.attributes.titles.en}</h3>
+                <img class="anime-img" src = "${item.attributes.posterImage.small}" alt = "food"></br>
+                <p class="description">${item.attributes.description}</p>
+                
+            </div>
+    `;         
+    // const description = document.querySelector('.description')
+    alert("You clicked the details button")
+    extraDetailsContainer.style.display = 'block';
+    const closeBtn = document.querySelector('.close-btn')
+    closeBtn.addEventListener('click', closeExtraDetails)
+}
+
+function closeExtraDetails() {
+    document.querySelector('.extra-details-container').style.display = 'none';
+}
+
+
+
